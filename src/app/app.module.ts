4f87a1cf4c39db2b4da0716c18033feca51445c3
BENMOUSSA SALMA <salma.benmoudssa@gmail.com>
2023-10-09 23:12:54 +0100

    Product List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { ContainerComponent } from './container/container.component';
+import { SearchComponent } from './container/search/search.component';
+import { FormsModule } from '@angular/forms';
+import { ProductListComponent } from './container/product-list/product-list.component';
+import { ProductComponent } from './container/product-list/product/product.component';
+import { FilterComponent } from './container/product-list/filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopHeaderComponent,
+    TopMenuComponent,
+    MainMenuComponent,
+    ContainerComponent,
+    SearchComponent,
+    ProductListComponent,
+    ProductComponent,
+    FilterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
