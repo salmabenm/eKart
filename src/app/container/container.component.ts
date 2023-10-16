a356d8488819106cb3ab78f8911f40ff5e68808f
BENMOUSSA SALMA <salma.benmoudssa@gmail.com>
2023-10-16 10:47:38 +0100

    Product Detail

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-container',
@@ -35,6 +36,8 @@ export class ContainerComponent {
   }*/
   listOfString: string[]=['Mark','Steve','Merry','John','Serah'];
   searchText:string="";
+
+  @ViewChild(ProductListComponent) productListComponent: ProductListComponent;
   setSearchText(value: string){
     this.searchText = value;
   }
--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -11,10 +11,22 @@ export class SearchComponent {
   //1-Create an event (in child component)
   @Output()
   searchTextChange: EventEmitter<string> = new EventEmitter<string>();
+  
+  //Optional 2nd argument of @ViewChild()
+  //1.read: use it to read the different token from the queried elements.
+  //2.static: Determines when the query is resolves (,{static: true})
+  //          True is when the view is initialized (before the first change detection) for the first time.
+  //          False if you want it to be resolved after every change detection
+  @ViewChild('searchInput') searchInputE1: ElementRef; //par reference
+  
   onSearchTextChange(){
-    this.searchTextChange.emit(this.searchText);
+    
   }
-  updateSearchText(event: any){
-      this.searchText = event.target.value;
+  updateSearchText(){
+      //this.searchText = event.target.value;
+      //console.log(inputEl.value);
+      this.searchText = this.searchInputE1.nativeElement.value;//pour l'accees a la valeur stocker dans template var 
+      this.searchTextChange.emit(this.searchText);
   }
+
 }
